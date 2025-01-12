5a3a8fb9aa46e315f7fde04f6b98433daa6957e2
Anastasia <nastya.solovyeva.99@gmail.com>
2025-01-12 20:07:37 +0300

    Добавлена связь бинов

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
 public class Egg6 {
+
+    public Egg6(Needle7 needle){
+        this.needle=needle;
+    }
+
+    @Autowired
+    private Needle7 needle;
+
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
